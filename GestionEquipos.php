6372563d47dbaa7e2b61e3fb226031f487455810
VictorE97 <baiza.020697@gmail.com>
2021-03-24 12:06:21 -0600

    Update

--- GestionEquipos.php
+++ GestionEquipos.php
@@ -6,7 +6,7 @@
         header("Location: login.php");
     }
 
-    $nombre = $_SESSION['nombre'];
+    $nombre = $_SESSION['nombreUsuario'];
     //$tipo_equipo = $_SESSION['idEquipo'];
     //$tipo_perfil = $_SESSION['idPerfil'];
 
@@ -42,22 +42,7 @@ $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.php">EXAVER</a>
-            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-            
-            <!-- Navbar-->
-            <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $nombre; ?><i class="fas fa-user fa-fw"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">Configuracion</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="logout.php">Cerrar Sesion</a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
+    <?php include ('Menu/Navbar.php'); ?>
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
--- Usuarios.php
+++ Usuarios.php
@@ -53,7 +53,8 @@ $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             
             <!-- Navbar-->
-            <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <?php include ('Menu/Navbar.php'); ?>
+            <!--ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $nombre; ?><i class="fas fa-user fa-fw"></i></a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
@@ -62,7 +63,7 @@ $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
                         <a class="dropdown-item" href="logout.php">Cerrar Sesion</a>
                     </div>
                 </li>
-            </ul>
+            </ul-->
         </nav>
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
